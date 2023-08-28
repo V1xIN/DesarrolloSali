5e5f5534b877ee02f378538ae7dc6a8307356f87
benjazul11 <be.vargasj@duocuc.cl>
2023-08-28 01:07:00 -0400

    Modificar usuario creado + recuperar contraseña
    - creación de la page de modificar el perfil del usuario => desplegar foto de perfil arriba centrada con la dirección
    - modificación de la pantalla de recuperar contraseña: enviar código
    - comentario en la pantalla de recuperar contraseña para ocupar en la page de modificar contraseña
    -

--- src/app/pages/modificaruser/modificaruser.page.ts
+++ src/app/pages/modificaruser/modificaruser.page.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificaruserPage implements OnInit {
 
+  selectedImage: any;
+
+  handleFileInput(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+
+
   constructor() { }
 
   ngOnInit() {
