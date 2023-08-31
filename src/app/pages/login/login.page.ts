a519e842fa32a32102b5f82750588f1ef99b9243
benjazul11 <be.vargasj@duocuc.cl>
2023-08-31 11:36:05 -0400

    Login y Register con formularios
    - Creación de formularios en Login y Register => SOBRE EL REGISTER ORIGINAL
    - Alerta en Register en caso de que los datos estén incompletos

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
+
+
+
+
+
+
+  constructor(public fb: FormBuilder) { 
+
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'passwprd': new FormControl("", Validators.required),
+
+    })
+      
+    
+    
+  }
 
   ngOnInit() {
   }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -19,9 +26,44 @@ export class RegisterPage implements OnInit {
     }
   }
 
-  constructor() { }
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder, public AlertController: AlertController) { 
+
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'apellido': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'rut': new FormControl("", Validators.required),
+      'mail': new FormControl("", Validators.required),
+      'confirmacionPassword': new FormControl("", Validators.required),
+      'celular': new FormControl("", Validators.required),
+      'direccion': new FormControl("", Validators.required),
+
+    })
+      
+    
+
+
+
+  }
 
   ngOnInit() {
   }
+
+  async guardar(){
+    var f = this.formularioRegistro.value;
+    if(this.formularioRegistro.invalid){
+      const alert = await this.AlertController.create({
+        header: 'Datos incompletos',
+        message: 'Por favor llenar todos los campos',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+    }
+  }
+
 
 }
