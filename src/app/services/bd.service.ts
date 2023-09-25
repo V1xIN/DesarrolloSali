89c99818899eb73278165ea11cc7b68cf4b72a27
benjazul11 <be.vargasj@duocuc.cl>
2023-09-24 21:16:30 -0300

    Creación base de datos
    Implementación del código del profe con el ejemplo de noticias
    Solo está implementado el código que crea la base de datos y las tablas
    FALTA CREAR LAS FUNCIONES DE CREAR, ELIMINAR Y EDITAR

--- src/app/services/bd.service.ts
+++ src/app/services/bd.service.ts
@@ -1,9 +1,82 @@
 import { Injectable } from '@angular/core';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Platform } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BDService {
 
-  constructor() { }
+  public database!: SQLiteObject;
+  tablaNoticia: string = "CREATE TABLE IF NOT EXISTS noticia ( id INTEGER PRIMARY KEY autoincrement, titulo VARCHAR(100) NOT NULL,texto VARCHAR(300) NOT NULL);";
+  registroNoticia: string = "INSERT OR IGNORE INTRO NOTICIA(id, titulo,texto) VALUES ( 1,  'Soy un título', 'Soy un texto largo de la noticia'); ";
+  listaNoticias = new BehaviorSubject([]);
+  private isDBREADY: BehaviorSubject<boolean> = new BehaviorSubject
+  (false);
+
+  
+  constructor(private alertController: AlertController, public sqlite: SQLite, private platform: Platform) {
+
+    this.crearBD();
+
+  }
+
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  crearBD(){
+    // verificar el platform
+   this.platform.ready().then(()=>{
+   //creamos la BD
+   this.sqlite.create({
+   name: 'bdnoticias.db',
+   location: 'default'
+   }).then((db: SQLiteObject)=>{
+     //capturar la coneccion a BD
+      this.database = db;
+   
+     // ejecuto la creación de tablas
+      this.crearTablas();
+   }).catch(e => {
+      this.presentAlert("Error en crearBD: " + e);
+   
+   })
+   
+   })
+   
+   
+   }
+
+
+   async crearTablas(){
+    try{
+    // ejecutar la creación de tablas
+    await this.database.executeSql(this.tablaNoticia,[]);
+    
+    //ejecuto los insert
+    await this.database.executeSql(this.registroNoticia,[]);
+    
+    
+    //cambio mi observable de BD
+    this.isDBREADY.next(true);
+    
+    }catch(e){
+      this.presentAlert("Error en crearBD: " + e);
+    }
+    
+    }
+    
 }
