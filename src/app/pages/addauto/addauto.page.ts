5d80a2aa740790b3e3791c38bfb6305991356cb4
DotoIbarra <ra.ibarra@duocuc.cl>
2023-10-08 21:18:40 -0300

    demas validaciones

--- src/app/pages/addauto/addauto.page.ts
+++ src/app/pages/addauto/addauto.page.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class AddautoPage implements OnInit {
 
   selectedImage: any;
+  modelo: string = '';
+  patente: string = '';
+  nroChasis: string = '';
+
+  errorMessages = {
+    modelo: '',
+    patente: '',
+    nroChasis: '',
+  };
 
   handleFileInput(event: any) {
     const file = event.target.files[0];
@@ -19,9 +28,63 @@ export class AddautoPage implements OnInit {
       reader.readAsDataURL(file);
     }
   }
+
+  validateModel(model: string): void {
+    if (!model || model.length < 1 || model.length > 12 || !/^[A-Za-z0-9]+$/.test(model)) {
+      this.errorMessages.modelo = 'El modelo debe tener entre 1 y 12 caracteres y puede incluir números y letras.';
+    } else {
+      this.errorMessages.modelo = '';
+    }
+  }
+
+  validatePatent(patent: string): void {
+    const regexPatent = /^[A-Z]{2}-[A-Z]{2}-[0-9]{2}$/;
+    if (!patent || !regexPatent.test(patent)) {
+      this.errorMessages.patente = 'La patente debe tener el formato xx-xx-99.';
+    } else {
+      this.errorMessages.patente = '';
+    }
+  }
+
+  validateChassis(chassis: string): void {
+    const regexChassis = /^[^IOQÑiñ]{1}[A-HJ-NPR-Z0-9]{4}-[A-HJ-NPR-Z0-9]{2}-[0-9]{1}-[A-HJ-NPR-Z0-9]{1}-[0-9]{1}-[0-9]{1}-[0-9]{1}-[0-9]{1}-[0-9]{1}-[0-9]{1}-[0-9]{1}$/;
+    if (!chassis || !regexChassis.test(chassis)) {
+      this.errorMessages.nroChasis = 'El número de chasis no es válido.';
+    } else {
+      this.errorMessages.nroChasis = '';
+    }
+  }
+
+  areAllValid(): boolean {
+    return (
+      this.selectedImage !== '' &&
+      this.errorMessages.modelo === '' &&
+      this.errorMessages.patente === '' &&
+      this.errorMessages.nroChasis === ''
+    );
+  }
+
+  agregarAuto(): void {
+    this.resetErrorMessages();
+
+    if (!this.areAllValid()) {
+      console.log("Las validaciones han fallado");
+      return;
+    }
+
+    console.log("Auto agregado exitosamente");
+  }
+
+  resetErrorMessages(): void {
+    this.errorMessages = {
+      modelo: '',
+      patente: '',
+      nroChasis: '',
+    };
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
