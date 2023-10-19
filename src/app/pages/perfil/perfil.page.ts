2a68b5f453390e1fd86b0da9b65910e6b26c67cb
DotoIbarra <ra.ibarra@duocuc.cl>
2023-10-19 16:50:07 -0300

    datos de registro a perfil
    tambien se cambiaron cosas en la bd para que funcionase

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,8 @@
+// perfil.page.ts
+
 import { Component, OnInit } from '@angular/core';
+import { BDService } from 'src/app/services/bd.service';
+import { Usuario } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
+  nombre: string = ''; // Asignar un valor inicial vacío
+  apellido: string = '';
+  rut: string = '';
+  email: string = '';
+  telefono: string = '';
+  direccion: string = '';
 
-  constructor() { }
+  constructor(private bdService: BDService) {}
 
   ngOnInit() {
-  }
+    // Cambia 'el-rut-del-usuario-registrado' con el rut del usuario actual
+    const rutUsuarioRegistrado = 'el-rut-del-usuario-registrado';
 
+    // Llama a tu servicio para obtener los datos del usuario
+    this.bdService.buscarUsuarioPorRut(rutUsuarioRegistrado).subscribe((usuario: Usuario[]) => {
+      if (usuario && usuario.length > 0) {
+        // Asigna los datos del usuario a las variables del componente
+        this.nombre = usuario[0].nombre;
+        this.apellido = usuario[0].apellido;
+        this.rut = usuario[0].rut;
+        this.email = usuario[0].correo;
+        this.telefono = usuario[0].telefono.toString(); // Convierte el número a cadena si es necesario
+        this.direccion = usuario[0].direccion;
+      }
+    });
+  }
 }
--- src/app/services/bd.service.ts
+++ src/app/services/bd.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 import { Platform } from '@ionic/angular';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Rol } from './rol.service';
 import { Auto } from './auto.service';
@@ -13,43 +12,39 @@ import { Usuario } from './usuario.service';
 import { Viaje } from './viaje.service';
 import { Detalle } from './detalle.service';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class BDService {
   public database!: SQLiteObject;
 
-
-  //TABLAS CREADAS//  
+  //TABLAS CREADAS//
   tablaRol: string =
-    'CREATE TABLE IF NOT EXISTS rol (idrol INTEGER PRIMARY KEY autoincrement NOT NULL, nombrerol VARCHAR (30) NOT NULL);';
+    'CREATE TABLE IF NOT EXISTS rol (idrol INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombrerol VARCHAR (30) NOT NULL);';
 
   tablaComuna: string =
-    'CREATE TABLE IF NOT EXISTS comuna (idcomuna INTEGER PRIMARY KEY autoincrement NOT NULL, nombreComuna VARCHAR(30)NOT NULL);';
+    'CREATE TABLE IF NOT EXISTS comuna (idcomuna INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombreComuna VARCHAR(30) NOT NULL);';
 
   tablaSedes: string =
-    'CREATE TABLE IF NOT EXISTS sede (idSede INTEGER PRIMARY KEY autoincrement NOT NULL, nombreSede VARCHAR(30)NOT NULL);';
+    'CREATE TABLE IF NOT EXISTS sede (idSede INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombreSede VARCHAR(30) NOT NULL);';
 
   tablaUsuario: string =
-    'CREATE TABLE IF NOT EXISTS usuario ( rut VARCHAR(12) PRIMARY KEY , nombre VARCHAR(100) NOT NULL,apellido VARCHAR(100) NOT NULL, correo VARCHAR(100) NOT NULL, clave VARCHAR(100) NOT NULL, telefono INTEGER(9) NOT NULL, direccion VARCHAR(50) NOT NULL,idrol_FK INTEGER, FOREING KEY (idrol_FK) REFERENCES rol(idrol) );';
+    'CREATE TABLE IF NOT EXISTS usuario (rut VARCHAR(12) PRIMARY KEY, nombre VARCHAR(100) NOT NULL, apellido VARCHAR(100) NOT NULL, correo VARCHAR(100) NOT NULL, clave VARCHAR(100) NOT NULL, telefono INTEGER NOT NULL, direccion VARCHAR(50) NOT NULL, idrol_FK INTEGER, FOREIGN KEY (idrol_FK) REFERENCES rol(idrol));';
 
   tablaAuto: string =
-    'CREATE TABLE IF NOT EXISTS auto (idAuto INTEGER PRIMARY KEY autoincrement NOT NULL,patente VARCHAR(20) NOT NULL, color VARCHAR(10) NOT NULL, marca VARCHAR(20) NOT NULL, modelo VARCHAR(20) NOT NULL, numeroChasis VARCHAR(100) NOT NULL,rut_FK VARCHAR(12),FOREING KEY (rut_FK) REFERENCES usuario(rut)) ;';
-  
+    'CREATE TABLE IF NOT EXISTS auto (idAuto INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, patente VARCHAR(20) NOT NULL, color VARCHAR(10) NOT NULL, marca VARCHAR(20) NOT NULL, modelo VARCHAR(20) NOT NULL, numeroChasis VARCHAR(100) NOT NULL, rut_FK VARCHAR(12), FOREIGN KEY (rut_FK) REFERENCES usuario(rut));';
+
   tablaViaje: string =
-    'CREATE TABLE IF NOT EXISTS viaje (idViaje INTEGER PRIMARY KEY NOT NULL , fechaViaje DATE NOT NULL, horaViaje DATE NOT NULL, asientos NUMBER(2) NOT NULL, costo INTEGER NOT NULL,idAuto_FK INTEGER, FOREING KEY (idAuto_FK) REFERENCES auto(idAuto), idSede_FK INTEGER, FOREING KEY (idSede_FK) REFERENCES sede(idSede), idcomuna_FK INTEGER, FOREING KEY (idcomuna_FK) REFERENCES comuna(idcomuna));';  // TABLA CON FK INCOMPLETA
-  
+    'CREATE TABLE IF NOT EXISTS viaje (idViaje INTEGER PRIMARY KEY NOT NULL, fechaViaje DATE NOT NULL, horaViaje DATE NOT NULL, asientos INTEGER NOT NULL, costo INTEGER NOT NULL, idAuto_FK INTEGER, idSede_FK INTEGER, idcomuna_FK INTEGER, FOREIGN KEY (idAuto_FK) REFERENCES auto(idAuto), FOREIGN KEY (idSede_FK) REFERENCES sede(idSede), FOREIGN KEY (idcomuna_FK) REFERENCES comuna(idcomuna));';
+
   tablaReclamo: string =
-    'CREATE TABLE IF NOT EXISTS reclamo (idReclamo INTEGER PRIMARY KEY autoincrement NOT NULL, descripcionReclamo VARCHAR(100) NOT NULL,idViaje_FK INTEGER,FOREING KEY (idViaje_FK) REFERENCES viaje(idViaje));';
+    'CREATE TABLE IF NOT EXISTS reclamo (idReclamo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, descripcionReclamo VARCHAR(100) NOT NULL, idViaje_FK INTEGER, FOREIGN KEY (idViaje_FK) REFERENCES viaje(idViaje));';
 
   tablaDetalle: string =
-    'CREATE TABLE IF NOT EXISTS detalleviaje (idDetalle INTEGER PRIMARY KEY autoincrement NOT NULL, calificacion INTEGER NOT NULL,rut_FK VARCHAR(12),FOREING KEY (rut_FK) REFERENCES usuario(rut),idViaje_FK INTEGER,FOREING KEY (idViaje_FK) REFERENCES viaje(idViaje));';
+    'CREATE TABLE IF NOT EXISTS detalleviaje (idDetalle INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, calificacion INTEGER NOT NULL, rut_FK VARCHAR(12), idViaje_FK INTEGER, FOREIGN KEY (rut_FK) REFERENCES usuario(rut), FOREIGN KEY (idViaje_FK) REFERENCES viaje(idViaje));';
   // FIN DE CREACIÓN DE TABLAS //
 
-
   // COMIENZO DE LOS INSERT //
-
   insertRol1: string =
     "INSERT OR IGNORE INTO rol (idrol, nombrerol) VALUES (1, 'Pasajero');";
 
@@ -77,22 +72,22 @@ export class BDService {
     "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (6, 'Cerrillos');";
 
   insertComuna7: string =
-    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (7, 'Melipilla');";  
+    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (7, 'Melipilla');";
 
   insertComuna8: string =
-    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (8, 'La Florida');";  
+    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (8, 'La Florida');";
 
   insertComuna9: string =
-    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (9, 'Puente Alto');";  
+    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (9, 'Puente Alto');";
 
   insertComuna10: string =
-    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (10, 'San Bernardo');";  
+    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (10, 'San Bernardo');";
 
   insertComuna11: string =
-    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (11, 'Las Condes');";  
-    
+    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (11, 'Las Condes');";
+
   insertComuna12: string =
-    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (12, 'San Joaquín');";       
+    "INSERT OR IGNORE INTO comuna (idComuna, nombreComuna) VALUES (12, 'San Joaquín');";
 
   //separador
 
@@ -103,7 +98,7 @@ export class BDService {
     "INSERT OR IGNORE INTO sede (idSede, nombreSede) VALUES (2, 'Sede Alameda');";
 
   insertSede3: string =
-    "INSERT OR IGNORE INTO sede (idSede, nombreSede) VALUES (3, 'Sede Padre Alonso de Ovalle');";  
+    "INSERT OR IGNORE INTO sede (idSede, nombreSede) VALUES (3, 'Sede Padre Alonso de Ovalle');";
 
   insertSede4: string =
     "INSERT OR IGNORE INTO sede (idSede, nombreSede) VALUES (4, 'Sede Antonio Varas');";
@@ -135,117 +130,133 @@ export class BDService {
   insertSede13: string =
     "INSERT OR IGNORE INTO sede (idSede, nombreSede) VALUES (13, 'Sede San Joaquín');";
 
- 
-
   //Funciones que retornan los observables
-  listaRol = new BehaviorSubject([]);
-  listaComuna = new BehaviorSubject([]);
-  listaSedes = new BehaviorSubject([]);
-  listaUsuario = new BehaviorSubject([]);
-  listaViaje = new BehaviorSubject([]);
-  listaReclamo = new BehaviorSubject([]);
-  listaAuto = new BehaviorSubject([]);
-  listaDetalle = new BehaviorSubject([]);
+  listaRol = new BehaviorSubject<Rol[]>([]);
+  listaComuna = new BehaviorSubject<Comuna[]>([]);
+  listaSedes = new BehaviorSubject<Sedes[]>([]);
+  listaUsuario = new BehaviorSubject<Usuario[]>([]);
+  listaViaje = new BehaviorSubject<Viaje[]>([]);
+  listaReclamo = new BehaviorSubject<Reclamo[]>([]);
+  listaAuto = new BehaviorSubject<Auto[]>([]);
+  listaDetalle = new BehaviorSubject<Detalle[]>([]);
 
   private isDBReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  constructor(private alertController: AlertController,public sqlite: SQLite,private platform: Platform){
+  constructor(private alertController: AlertController, public sqlite: SQLite, private platform: Platform) {
     this.crearBD();
   }
 
-  bdState(){
+  bdState() {
     return this.isDBReady.asObservable();
   }
 
-  fetchRol(): Observable<Rol[]>{
+  fetchRol(): Observable<Rol[]> {
     return this.listaRol.asObservable();
   }
 
-  fetchComuna(): Observable<Comuna[]>{
+  fetchComuna(): Observable<Comuna[]> {
     return this.listaComuna.asObservable();
   }
 
-  fetchSedes(): Observable<Sedes[]>{
-    return this.listaComuna.asObservable();
+  fetchSedes(): Observable<Sedes[]> {
+    return this.listaSedes.asObservable();
   }
 
-  fetchUsuario(): Observable<Usuario[]>{
+  fetchUsuario(): Observable<Usuario[]> {
     return this.listaUsuario.asObservable();
   }
 
-  fetchViaje(): Observable<Viaje[]>{
+  fetchViaje(): Observable<Viaje[]> {
     return this.listaViaje.asObservable();
   }
 
-  fetchReclamo(): Observable<Reclamo[]>{
+  fetchReclamo(): Observable<Reclamo[]> {
     return this.listaReclamo.asObservable();
   }
-  fetchAuto(): Observable<Auto[]>{
+  fetchAuto(): Observable<Auto[]> {
     return this.listaAuto.asObservable();
   }
 
-  fetchDetalle(): Observable<Detalle[]>{
+  fetchDetalle(): Observable<Detalle[]> {
     return this.listaDetalle.asObservable();
   }
-//Fin Funciones que retornan los observables
-//Funcion para buscar datos en las tabla sede
-buscarSedes(){
-  return this.database.executeSql('SELECT * FROM sede',[]).then(res=>{
-    //variable para almacenar el resultado
-    let items: Sedes[] = [];
-    //verfico la cantidad de registros
-    if(res.rows.length > 0){
-      //agrego registro a registro en mi variable
-      for(var i=0; i < res.rows.length; i++){
-        items.push({
-          idSede: res.rows.item(i).idSede,
-          nombreSede: res.rows.item(i).nombreSede
-        })
+
+  buscarSedes() {
+    return this.database.executeSql('SELECT * FROM sede', []).then(res => {
+      //variable para almacenar el resultado
+      let items: Sedes[] = [];
+      //verifico la cantidad de registros
+      if (res.rows.length > 0) {
+        //agrego registro a registro en mi variable
+        for (var i = 0; i < res.rows.length; i++) {
+          items.push({
+            idSede: res.rows.item(i).idSede,
+            nombreSede: res.rows.item(i).nombreSede,
+          });
+        }
       }
-    }
-    //actualizo el observable
-    this.listaSedes.next(items as any);
-  })
-}
-//Fin funcion que busca datos en la tabla rol
-buscarRol(){
-  return this.database.executeSql('SELECT * FROM rol',[]).then(res=>{
-    //variable para almacenar el resultado
-    let roles: Rol[] = [];
-    //verfico la cantidad de registros
-    if(res.rows.length > 0){
-      //agrego registro a registro en mi variable
-      for(var i=0; i < res.rows.length; i++){
-        roles.push({
-          idrol: res.rows.item(i).idrol,
-          nombrerol: res.rows.item(i).nombrerol
-        })
+      //actualizo el observable
+      this.listaSedes.next(items);
+    });
+  }
+
+  buscarRol() {
+    return this.database.executeSql('SELECT * FROM rol', []).then(res => {
+      //variable para almacenar el resultado
+      let roles: Rol[] = [];
+      //verifico la cantidad de registros
+      if (res.rows.length > 0) {
+        //agrego registro a registro en mi variable
+        for (var i = 0; i < res.rows.length; i++) {
+          roles.push({
+            idrol: res.rows.item(i).idrol,
+            nombrerol: res.rows.item(i).nombrerol,
+          });
+        }
       }
-    }
-    //actualizo el observable
-    this.listaRol.next(roles as any);
-  })
-}
-//Fin funcion que busca datos en la tabla rol
+      //actualizo el observable
+      this.listaRol.next(roles);
+    });
+  }
 
+  buscarUsuarioPorRut(rut: string): Observable<Usuario[]> {
+    return new Observable((observer) => {
+      this.database
+        .executeSql('SELECT * FROM usuario WHERE rut = ?', [rut])
+        .then((data) => {
+          if (data.rows.length > 0) {
+            let usuarios: Usuario[] = [];
+            for (let i = 0; i < data.rows.length; i++) {
+              usuarios.push({
+                rut: data.rows.item(i).rut,
+                nombre: data.rows.item(i).nombre,
+                apellido: data.rows.item(i).apellido,
+                correo: data.rows.item(i).correo,
+                clave: data.rows.item(i).clave,
+                telefono: data.rows.item(i).telefono,
+                direccion: data.rows.item(i).direccion,
+                idrol_FK: data.rows.item(i).idrol_FK,
+              });
+            }
+            observer.next(usuarios);
+          } else {
+            observer.next([]);
+          }
+          observer.complete();
+        })
+        .catch((error) => {
+          this.presentAlert('Error al buscar usuario por rut: ' + error);
+          observer.error(error);
+          observer.complete();
+        });
+    });
+  } 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  async presentAlert(msj:String) {
+  async presentAlert(msj: string) {
     const alert = await this.alertController.create({
       header: 'Alert',
       subHeader: 'Important message',
-      message: 'This is an alert!',
+      message: msj,
       buttons: ['OK'],
     });
 
@@ -254,21 +265,24 @@ buscarRol(){
 
   crearBD() {
     // verificar el platform
-    this.platform.ready().then(()=>{
+    this.platform.ready().then(() => {
       //creamos la BD
-      this.sqlite.create({
+      this.sqlite
+        .create({
           name: 'bdsali.db',
           location: 'default',
-        }).then((db:SQLiteObject)=>{
+        })
+        .then((db: SQLiteObject) => {
           //capturar la coneccion a BD
           this.database = db;
 
           // ejecuto la creación de tablas
           this.crearTablas();
-        }).catch(e =>{
-          this.presentAlert("Error en crearBD: " + e);
         })
-    })
+        .catch((e) => {
+          this.presentAlert('Error en crearBD: ' + e);
+        });
+    });
   }
 
   async crearTablas() {
@@ -281,7 +295,7 @@ buscarRol(){
       await this.database.executeSql(this.tablaViaje, []);
       await this.database.executeSql(this.tablaReclamo, []);
       await this.database.executeSql(this.tablaAuto, []);
-      await this.database.executeSql(this.tablaDetalle, [])
+      await this.database.executeSql(this.tablaDetalle, []);
 
       //ejecuto los insert
       await this.database.executeSql(this.insertRol1, []);
